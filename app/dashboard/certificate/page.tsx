4ddc0d87b6dde41b50687ee3ffebe1643642b294
김지성 <js.kim@eunwoosoft.com>
2026-03-06 15:35:12 +0900

    complete: NestJS 튜토리얼 [대시보드 만들기] 완료

---
+++ app/dashboard/certificate/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <Image
+      src="/certificate/dashboard-app-certificate.jpg"
+      width={1000}
+      height={760}
+      className="hidden md:block"
+      alt="Certificate of the Vercel dashboard app course"
+    />
+  );
+}
